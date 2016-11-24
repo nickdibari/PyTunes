d78be338b45aea4b5651ecc3df60180fde23d0ea
nickdibari <ndibari@fordham.edu>
2016-11-23 23:01:20 -0500

    Cleaned up ArtistExplorer

--- ArtistExplorer/ArtistExplorer.py
+++ ArtistExplorer/ArtistExplorer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.4                            
+#!/usr/bin/python
 
 #* Nicholas DiBari, Chris George, & Matt Satalino                   *#
 #* Artist Explorer                                                  *#
@@ -11,10 +11,9 @@
 #* TODO                                                             *#
 #* Use Spotify API to suggest new artists based on previous entries *#
 
-import webbrowser, sys, os, shelve
-
-for i in range(20):
-    print(" ")
+import webbrowser
+import sys
+import shelve
 
 #Creates database file of artists to be entered
 artistFile = shelve.open('Entries')
@@ -25,46 +24,47 @@ prev_entries.sort()
 
 #Prompts user to decide which option to choose
 while(True):
-    choice=input("Please decide which input you would like.\n1.Enter a new artist\n2.View previous entries\n")
+    choice=raw_input('Please decide which input you would like.\n1.Enter a new artist\n2.View previous entries\n')
 
-    if choice == ("1"):
+    if choice == ('1'):
         #Get artist to search
-        artist=input("Please enter an artist to search for: ")
+        artist=raw_input('Please enter an artist to search for: ')
         break
     
-    elif choice == ("2"):
+    elif choice == ('2'):
         #Show user previous entries
-        print("You have previously entered:")
+        print('You have previously entered:')
         for i in range(len(prev_entries)):
             print (str(i+1) + ('. ') + prev_entries[i])
-        choice=input("Which artist would you like to see? Please choose the number: ")
+        choice=raw_input('Which artist would you like to see? Please choose the number: ')
         choice = int(choice)
         while choice>len(prev_entries) or choice<0:
-            choice=input("Not a valid input. Please enter a number within the range of the list: ")
+            choice=raw_input('Not a valid input. Please enter a number within the range of the list: ')
         choice = choice-1
         artist = prev_entries[choice]
         break
+    
     else:
-        choice=input("That is an invalid input. Please input 1 for a new artist or 2 to view previous entries ")
+        choice=raw_input('That is an invalid input. Please input 1 for a new artist or 2 to view previous entries ')
 #If user wants to, opens ultimate guitar page as well
-choice=input("Do you want to see the guitar explorer page for " +artist +"? (y/n) ")
-print("Loading...")
+choice=raw_input('Do you want to see the guitar explorer page for ' +artist +'? (y/n) ')
+print('Loading...')
 
 #Genius page
-webbrowser.open('http://genius.com/search?q=' + artist)
+webbrowser.open('http://genius.com/search?q={0}'.format(artist))
 
 #Wikipedia page
-webbrowser.open('https://en.wikipedia.org/wiki/' + artist)
+webbrowser.open('https://en.wikipedia.org/wiki/{0}'.format(artist))
 
 #Youtube Page
-webbrowser.open('https://www.youtube.com/results?search_query=' + artist)
+webbrowser.open('https://www.youtube.com/results?search_query={0}'.format(artist))
 
 #Soundcloud Page
-webbrowser.open('https://soundcloud.com/search?q=' + artist)
+webbrowser.open('https://soundcloud.com/search?q={0}'.format(artist))
 
 #Ultimate Guitar Page
 if choice == 'y' or choice == 'Y':
-    webbrowser.open('http://www.ultimate-guitar.com/search.php?search_type=title&value=' + artist)
+    webbrowser.open('http://www.ultimate-guitar.com/search.php?search_type=title&value={0}'.format(artist))
 
 #Enters artist entered into the list of previous entries
 if artist not in prev_entries:
@@ -72,7 +72,6 @@ if artist not in prev_entries:
 
 #Writes new list of previous entries to the data file
 for a in range(len(prev_entries)):
-    artistFile["Artist:" + str(a)]=prev_entries[a]
+    artistFile['Artist:' + str(a)]=prev_entries[a]
+
 artistFile.close()
-
-
