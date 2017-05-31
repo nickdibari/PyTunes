3dc9cca0b6774a7524e7c3912c57db52f7e5a0bf
nickdibari <ndibari@fordham.edu>
2017-05-31 10:24:33 -0400

    Refactored ScaleFinder

--- ScaleFinder/ScaleFinder.py
+++ ScaleFinder/ScaleFinder.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3.4
+#! /usr/bin/python
 
 #* Nicholas DiBari & Chris George                                                     *#
 #* ScaleFinder                                                                        *#
@@ -20,26 +20,26 @@ import shelve
 
 
 # 1. Creates list of scales from database
-scalefile=shelve.open("Scales")
+scalefile=shelve.open("Scales.db")
 scales=list(scalefile.values())
 scales.sort()
     
+
 # 2. Prompts User for input
 def GetNote():
-
-
     chord = ' '
     chords_entered = []
-    for a in range(20):
-        print (" ")
+
     print ('Please pick your input choices: ')
     print ('1. Display specific scale')
     print ('2. Enter multiple chords to find scale')
-    choice = int(input())
+    choice = raw_input('> ')
+
     while True:
+        choice = int(choice)
         #Position/Chord Initialized
         if choice == 1:
-            chord = input("Please input what scale you would like to view " )
+            chord = raw_input("Please input what scale you would like to view " )
             CheckScalePOS(chord)
             break
         
@@ -48,7 +48,7 @@ def GetNote():
         if choice == 2:
             print ("Please input the chords in your scale (enter done to exit)")
             for b in range(6):
-                entry = input()
+                entry = raw_input()
                 if entry == 'done':
                     break
                 chords_entered.append(entry)
@@ -56,7 +56,7 @@ def GetNote():
             break
         
         else:
-            choice=input("That's not a valid input. Please try again")
+            choice=raw_input("That's not a valid input. Please try again")
         
 
 # 3A. Checks Input against scales List (POS/CHORD)
@@ -73,22 +73,25 @@ def CheckScaleCHORDS(chords_entered):
         if set(chords_entered) < set(hold):
             DisplayScale(hold)
 
-    
 
 # 4. Displays required scale
 def DisplayScale(found):
-    print (' ')
-    print ("You're in the " + found[0] + " scale:")
-    for i in range(len(found)):
-        print(found[i])
-    print(" ")
+    print ('You are in the {} scale:'.format(found[0]))
+
+    for chord in found:
+        print(chord)
     
     #If user chooses, also displays the relative Minor scale
-    choice = input("Would you like to see the relative minor scale for " + found[0] + "? (y/n): ")
-    print(" ")
+    prompt = 'Would you like to see the relative minor scale for {}? (y/n): '\
+             .format(found[0])
+
+    choice = raw_input(prompt)
+
     if choice == 'y' or choice == 'Y':
-        print("The " + found[-2] + " scale:")
+        print('The {} scale: '.format(found[-2]))
         for i in range(len(found)):
             print(found[i-2])
 
-GetNote()
+
+if __name__ == '__main__':
+    GetNote()
